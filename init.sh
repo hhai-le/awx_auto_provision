e03381fa2d12542230fa8947f412f29020adb74c
hhai-le <69373181+hhai-le@users.noreply.github.com>
2024-05-31 10:29:13 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl make build-essential
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+#curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o docker-compose
+#sudo mkdir -p /usr/local/lib/docker/cli-plugins/
+#sudo mv docker-compose /usr/local/lib/docker/cli-plugins/docker-compose
+#chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+sudo usermod -aG docker linuxroot
+newgrp docker
+
 mkdir -p ./gitlab/{config,logs,data}
 mkdir -p ./vault/{config,data}
 mkdir -p ./jenkins/jenkins_home
